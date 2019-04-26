29d9af66627831b70b2d4ec5468e284f2061b996
Samuel <samuelsoutullosobral@gmail.com>
2019-04-26 23:32:34 +0200

    Added smooth camera follow to leader (in Samuel's scene)

--- NGJ2019/Assets/Scripts/Leader.cs
+++ NGJ2019/Assets/Scripts/Leader.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Leader : MonoBehaviour
 {
-    public float Speed = 10.0f;
+    public float Speed = 0.125f;
 
     Rigidbody rb = null;
+
+    // Camera parameters
+    public float cameraSmoothSpeed;
+    public Vector3 cameraOffset;
+    private Camera MainCamera;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        MainCamera = Camera.main;
     }
 
     void Update()
@@ -20,7 +26,21 @@ public class Leader : MonoBehaviour
 
     private void FixedUpdate()
     {
-            rb.AddForce(0.0f, 0.0f, Speed * Input.GetAxis("Vertical"));
-            rb.AddForce(Speed * Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        rb.AddForce(0.0f, 0.0f, Speed * Input.GetAxis("Vertical"));
+        rb.AddForce(Speed * Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+    }
+
+    private void LateUpdate()
+    {
+        CameraFollow();
+    }
+
+    private void CameraFollow()
+    {
+        Vector3 desiredPosition = transform.position + cameraOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(MainCamera.transform.position, desiredPosition, cameraSmoothSpeed * Time.deltaTime * 60);
+
+        MainCamera.transform.position = smoothedPosition;
+        MainCamera.transform.LookAt(transform);
     }
 }
