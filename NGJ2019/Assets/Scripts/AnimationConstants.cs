c94c6f4519617d66d8d12e164d704b1cf14b10bd
OLianoudakis <gusliaor@student.gu.se>
2019-04-27 00:21:41 +0200

    all egg types

---
+++ NGJ2019/Assets/Scripts/AnimationConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationConstants : MonoBehaviour
+{
+    public static string beTall = "BeTall";
+    public static string animatorSpeed = "Speed";
+}
