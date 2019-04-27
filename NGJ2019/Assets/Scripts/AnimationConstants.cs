612d80b18a315c2318c1e60425ce9b7ce4593290
OLianoudakis <gusliaor@student.gu.se>
2019-04-27 23:48:31 +0200

    tank Charge animation

--- NGJ2019/Assets/Scripts/AnimationConstants.cs
+++ NGJ2019/Assets/Scripts/AnimationConstants.cs
@@ -6,4 +6,5 @@ public class AnimationConstants : MonoBehaviour
 {
     public static string beTall = "BeTall";
     public static string animatorSpeed = "Speed";
+    public static string doCharge = "DoCharge";
 }
