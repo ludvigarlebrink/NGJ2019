162605f8a01ddad42905aa992286e8426bbab85d
Christopher <chris.wblack@live.de>
2019-04-27 19:38:31 +0200

    ability triggers

---
+++ NGJ2019/Assets/AbilityTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityTrigger : MonoBehaviour
+{
+    public Egg.Type EggType;
+
+    private EggArmy army;
+    private int inReach = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        army = FindObjectOfType<EggArmy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (inReach > 0)
+            {
+                
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Egg egg = other.GetComponent<Egg>();
+        if (egg)
+        {
+            if (egg.type == EggType) ++inReach;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Egg egg = other.GetComponent<Egg>();
+        if (egg)
+        {
+            if (egg.type == EggType) --inReach;
+        }
+    }
+}
