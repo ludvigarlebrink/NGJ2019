541915c3ae77944a739272a421bd27b708303461
OLianoudakis <gusliaor@student.gu.se>
2019-04-27 22:44:00 +0200

    resolving conflicts

---
+++ NGJ2019/Assets/PlastisBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlastisBehaviour : MonoBehaviour
+{
+    public float rotSpeed = 60.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotSpeed * Time.deltaTime);
+    }
+}
+
