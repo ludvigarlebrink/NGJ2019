f601b9b8ac4f30bb30d94cfa7f48ff3389783319
Samuel <samuelsoutullosobral@gmail.com>
2019-04-27 17:13:20 +0200

    Added game over screen

---
+++ NGJ2019/Assets/Scripts/GameOverCameraBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverCameraBehaviour : MonoBehaviour
+{
+    public float cameraSpeed = 20;
+    public Vector3 cameraDistance = new Vector3(40, 60, 40);
+    public Vector3 cameraLookAt = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(cameraLookAt + cameraDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(cameraLookAt);
+        transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+    }
+}
