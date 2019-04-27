909891594c98674841d5c0f90f7d3b52607f249b
travisko <marek.cern@gmail.com>
2019-04-27 23:34:34 +0200

    Fixed time to go backwards.

--- NGJ2019/Assets/Scripts/Timer.cs
+++ NGJ2019/Assets/Scripts/Timer.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    private const float timeInterval = 10000.0f;
     private float time;
     EggArmy eggArmy;
     // Start is called before the first frame update
     void Start()
     {
-        time = 0.0f;
+        time = 1000.0f;
         eggArmy = FindObjectOfType<EggArmy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        time -= Time.deltaTime;
 
-        if (time >= timeInterval)
+        if (time <= 0)
         {
             eggArmy.Restart();
-            time = 0.0f;
+            time = 1000.0f;
         }
     }
 
