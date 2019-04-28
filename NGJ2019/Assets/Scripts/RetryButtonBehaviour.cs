ced8518e4e2fffe1ad160f9abcde5f9be8e27e7e
Samuel <samuelsoutullosobral@gmail.com>
2019-04-28 05:22:54 +0200

    Added victory screen

---
+++ NGJ2019/Assets/Scripts/RetryButtonBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetryButtonBehaviour : MonoBehaviour
+{
+    public void OnButtonClicked()
+    {
+        Debug.Log("Retry Button Clicked!");
+        // TODO add retry logic here
+    }
+}
